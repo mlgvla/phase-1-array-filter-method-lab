181798ae95772f9900134e9698525bb77dcb93cd
mlgvla <mlgerardvla@gmail.com>
2022-01-12 15:31:29 -0500

    solutions

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+function findMatching(driversNames, name) {
+  return driversNames.filter(
+    (driverName) => driverName.toLowerCase() === name.toLowerCase()
+  );
+}
+
+function fuzzyMatch(driversNames, string) {
+  return driversNames.filter((driverName) =>
+    driverName.toLowerCase().indexOf(string.toLowerCase()) === 0
+  );
+  // can also use startsWith(string)
+}
+
+function matchName(drivers, string) {
+   return drivers.filter((driver) => driver.name === string )
+}
